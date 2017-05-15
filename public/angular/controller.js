0d0542ead1c6a28543ae094a48f8c1e38955b4f1
weiwenda <weiwenda@stu.xjtu.edu.cn>
2017-05-15 21:54:20 +0800

    Signed-off-by: weiwenda <weiwenda@stu.xjtu.edu.cn>

--- public/angular/controller.js
+++ public/angular/controller.js
@@ -115,19 +115,16 @@ function() {
 
         if (angular.isArray(items)) {
             var keys = Object.keys(props);
-
             items.forEach(function(item) {
                 var itemMatches = false;
 
                 for (var i = 0; i < keys.length; i++) {
                     var prop = keys[i];
-                    if(props[prop]){
-                        var text = props[prop].toLowerCase();
-                    if (item[prop].toString().toLowerCase().indexOf(text) !== -1) {
+                    var text = props[prop].toLowerCase();
+                    if(item[prop] && item[prop].toString().toLowerCase().indexOf(text) !== -1) {
                         itemMatches = true;
                         break;
-                        }
-                    }    
+                        }  
                 }
 
                 if (itemMatches) {
