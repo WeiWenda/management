ccf56ad1d99adbfbd9bdf028fcd3429ffbf0d5bf
weiwenda <weiwenda@stu.xjtu.edu.cn>
2017-05-15 18:50:31 +0800

    Signed-off-by: weiwenda <weiwenda@stu.xjtu.edu.cn>

--- public/angular/controller.js
+++ public/angular/controller.js
@@ -39,7 +39,7 @@ function($http) {
 }]);
 cjsApp.factory('webSocketData',
 function() {
-    var ws = io.connect('http://127.0.0.1:8888');
+    var ws = io.connect('/');
     ws.on('connect',
     function(msg) {});
     var logs = [];
