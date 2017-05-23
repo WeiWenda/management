de02483d3ea07d31887ea11ea3af3c5e281fde03
weiwenda <weiwenda@stu.xjtu.edu.cn>
2017-05-23 19:40:44 +0800

    Signed-off-by: weiwenda <weiwenda@stu.xjtu.edu.cn>

--- models/DBOpt.js
+++ models/DBOpt.js
@@ -258,7 +258,7 @@ var DBOpt = {
 
              })
             }
-            if(obj === Paper || obj === Patent  || obj === SoftwareCopyright    || obj === Award || obj === adminUser || obj === adminGroup){
+            if(obj === Paper || obj === Patent  || obj === SoftwareCopyright    || obj === Award || obj === AdminUser || obj === AdminGroup){
                  obj.remove({_id : params.query.uid},function(err,result){
                         if(err){
                             res.end(err);
